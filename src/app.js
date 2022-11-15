c32b574e02f9173a4c85f45d5c401f4b0dab0f74
dm8262a <114957367+dm8262a@users.noreply.github.com>
2022-11-15 16:23:47 -0500

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let descriptionHumidity = document.querySelector("#humidity");
+  let descriptionWind = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  descriptionHumidity.innerHTML = response.data.main.humidity;
+  descriptionWind.innerHTML = Math.round(response.data.wind.speed);
 }
+
 let apiKey = "6df7e1335a2ad82c3029b7821a8d1f8c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
