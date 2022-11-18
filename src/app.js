d9e0e5e2cf71bb851cae4f382503c888b5dea167
dm8262a <114957367+dm8262a@users.noreply.github.com>
2022-11-17 20:00:37 -0500

    added CSS styling corrected JS for "feels-like" description

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let feelsLikeElement = document.querySelector("#feels-cel-link");
+  let feelsLikeElement = document.querySelector("#feels-like");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let conditionIconElement = document.querySelector("#conditionIcon");
