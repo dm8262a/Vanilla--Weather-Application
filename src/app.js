b1ec7525528b8f5fcc29c00cfd021c93e6be5ac2
dm8262a <114957367+dm8262a@users.noreply.github.com>
2022-11-20 11:30:04 -0500

    get vanilla weather forecast API

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ function formatDate(times) {
   return `${day}, ${month} ${todaysDate} (updated at: ${hours}:${minutes})`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast-temperatures");
 
   let days = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -68,6 +69,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function obtainForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -99,10 +108,12 @@ function displayTemperature(response) {
     "alt",
     response.data.weather[0].description
   );
+
+  obtainForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "6df7e1335a2ad82c3029b7821a8d1f8c";
+  let apiKey = "aa09763d916df0424c840d55bfc2d2c9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
@@ -159,4 +170,3 @@ let feelsCelLink = document.querySelector("#feels-cel-link");
 feelsCelLink.addEventListener("click", showFeelsCelTemp);
 
 search("New York");
-displayForecast();
